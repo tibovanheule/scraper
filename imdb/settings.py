d1defb01b1f93d76824346fe433854068ef01a6c
Tibo Vanheule <tibo@tibovanheule.space>
2024-03-22 23:55:57 +0100

    test

--- imdb/settings.py
+++ imdb/settings.py
@@ -88,6 +88,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "3.9"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
