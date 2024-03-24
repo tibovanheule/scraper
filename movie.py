5c2e8f926e9f836316343572e3f897e91856c2ae
Tibo Vanheule <tibo@tibovanheule.space>
2024-03-24 17:23:50 +0100

    test

---
+++ movie.py
@@ -0,0 +1,75 @@
+import re
+
+import scrapy
+from pymongo import MongoClient
+from scrapy import Request
+
+
+def get_id(url):
+    return re.match(r"^(https://)?(http://)?www\.imdb\.com/[a-z]*/(tt[0-9]*)/[a-zA-Z0-9?/_&=]*$", url).group(3)
+
+
+class IMDBSpider(scrapy.Spider):
+    name = 'imdb_spider'
+    start_urls = ("https://www.imdb.com/title/tt20850406/?ref_=tt_sims_tt_t_1", "https://www.imdb.com/title/tt0241527/",
+                  "https://www.imdb.com/title/tt0266543/?ref_=hm_tpks_tt_i_12_pd_tp1_pbr_ic",
+                  "https://www.imdb.com/title/tt2788316/?ref_=hm_top_tt_i_5",
+                  "https://www.imdb.com/title/tt10919420/?ref_=hm_tpks_tt_i_15_pd_tp1_pbr_ic",
+                  "https://www.imdb.com/title/tt0382932/?ref_=hm_tpks_tt_i_18_pd_tp1_pbr_ic",
+                  "https://www.imdb.com/title/tt1632701/?ref_=hm_tpks_tt_i_26_pd_tp1_pbr_ic",
+                  "https://www.imdb.com/title/tt14230458/?ref_=hm_top_tt_i_2",
+                  "https://www.imdb.com/title/tt15239678/?ref_=hm_top_tt_i_3",
+                  "https://www.imdb.com/title/tt0068646/?ref_=nv_sr_srsg_3_tt_4_nm_4_q_godf",
+                  "https://www.imdb.com/title/tt0108052/?ref_=hm_tpks_tt_i_4_pd_tp1_pbr_ic")
+    myclient = MongoClient("mongodb://localhost:27017/")
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    }
+
+    def parse(self, response):
+        # database
+        db = self.myclient["IMDB"]
+
+        # Created or Switched to collection
+        # names: GeeksForGeeks
+        collection = db["movies2"]
+        # iterate movie sections
+        data = {"_id": get_id(response.url)}
+        if collection.count_documents({'_id': data["_id"]}) == 0:
+            data['title'] = response.css('.hero__primary-text::text').get().strip()
+
+            genres = response.xpath(
+                '//*[@data-testid="genres"]/div[contains(@class,"ipc-chip-list__scroller")]/a/span/text()').extract() or None
+            if genres:
+                data['genre'] = [genre.strip() for genre in genres]
+            data['description'] = response.css('.hlbAws::text').get() or None
+            directors = response.xpath(
+                "//li[contains(.//span, 'Director')]/div/ul/li/a/text()").extract() or None
+            if directors:
+                data['directors'] = list(set(directors))
+
+            data["origin"] = response.xpath(
+                "//*[contains(.//*, 'Country of origin')]/div/ul/li/a/text()").extract() or None
+            data["Runtime"] = response.xpath(
+                "//*[contains(.//*, 'Runtime')]/div/text()").extract() or None
+            data["release date"] = response.xpath(
+                "//*[contains(.//*, 'Release date')]/div/ul/li/a/text()").extract() or None
+
+            writers = response.xpath(
+                '//*[contains(.//*, "Writers")]/div/ul/li/a/text()').extract() or None
+            if writers:
+                data['writers'] = list(set(writers))
+
+            stars = response.xpath(
+                "//*[contains(.//*, 'Stars')]/div/ul/li/a/text()").extract() or None
+            if stars:
+                data['stars'] = list(set(stars))
+            collection.insert_one(data)
+            del data
+            for link in response.css(".ipc-poster-card__title"):
+                url = f"https://www.imdb.com{link.attrib['href']}"
+                id = get_id(url)
+                if collection.count_documents({'_id': id}) < 1:
+                    del id
+                    yield Request(url, callback=self.parse)
+        yield None
--- ratings.py
+++ ratings.py
@@ -1,16 +1,23 @@
+import re
+
 import scrapy
 from pymongo import MongoClient
 from scrapy import Request
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import Rule
+
+
+def get_id(url):
+    return re.match(r"^(https://)?(http://)?www\.imdb\.com/title/(tt[0-9]*)/[a-zA-Z0-9?/_=&]*$", url).group(3)
 
 
 class RatingSpider(scrapy.Spider):
     myclient = MongoClient("mongodb://localhost:27017/")
     name = 'imdb_spider'
     data_url = None
-
-    start_urls = ("https://www.imdb.com/title/tt20850406/reviews/",)
+    db = myclient["IMDB"]["movies"]
+    start_urls = [f"https://www.imdb.com/title/{i['_id']}/reviews/" for i in db.find({})]
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    }
 
     def parse(self, response):
         if response.status is not 404:
@@ -26,23 +33,24 @@ class RatingSpider(scrapy.Spider):
                 # review title
                 movie_name = rating.css('.title::text').get().strip()
                 # movie year
-                movie_rating = rating.css('.rating-other-user-rating span::text').get().strip()
-
+                movie_rating = rating.css('.rating-other-user-rating span::text').get() or None
+                if movie_rating is None:
+                    continue
                 user = rating.css('.display-name-link a::text').get().strip()
 
                 date = rating.css('.review-date::text').get().strip()
                 text = rating.css('.content .text::text').get().strip()
-                movie_dict = {
+                ratings.append({
+                    '_id': f"{get_id(response.url)}_{user}",
                     'rating_title': movie_name,
                     'rating': movie_rating,
+                    'movie_id': get_id(response.url),
                     'date': date,
                     'text': text,
                     'user': user
-                }
-                ratings.append(movie_dict)
-            del rating
-            collection.insert_many(ratings)
-            del ratings, collection
+                })
+            if ratings is not None and len(ratings)>0:
+                collection.insert_many(ratings)
             data_key = None
             if "data-key" in response.css('.load-more-data').attrib:
                 data_key = response.css('.load-more-data').attrib["data-key"]
