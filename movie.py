9c00fa25fddf59a041db23212615d5a23e8e6931
Tibo Vanheule <mail@tibovanheule.space>
2024-04-03 09:52:50 +0200

    Update movie.py

--- movie.py
+++ movie.py
@@ -11,17 +11,22 @@ def get_id(url):
 
 class IMDBSpider(scrapy.Spider):
     name = 'imdb_spider'
-    start_urls = ("https://www.imdb.com/title/tt20850406/?ref_=tt_sims_tt_t_1", "https://www.imdb.com/title/tt0241527/",
-                  "https://www.imdb.com/title/tt0266543/?ref_=hm_tpks_tt_i_12_pd_tp1_pbr_ic",
-                  "https://www.imdb.com/title/tt2788316/?ref_=hm_top_tt_i_5",
-                  "https://www.imdb.com/title/tt10919420/?ref_=hm_tpks_tt_i_15_pd_tp1_pbr_ic",
-                  "https://www.imdb.com/title/tt0382932/?ref_=hm_tpks_tt_i_18_pd_tp1_pbr_ic",
-                  "https://www.imdb.com/title/tt1632701/?ref_=hm_tpks_tt_i_26_pd_tp1_pbr_ic",
-                  "https://www.imdb.com/title/tt14230458/?ref_=hm_top_tt_i_2",
-                  "https://www.imdb.com/title/tt15239678/?ref_=hm_top_tt_i_3",
-                  "https://www.imdb.com/title/tt0068646/?ref_=nv_sr_srsg_3_tt_4_nm_4_q_godf",
-                  "https://www.imdb.com/title/tt0108052/?ref_=hm_tpks_tt_i_4_pd_tp1_pbr_ic")
+    update = True
     myclient = MongoClient("mongodb://localhost:27017/")
+    if update:
+        start_urls = [f"https://www.imdb.com/title/{i['_id']}/?ref_=undefined" for i in myclient["IMDB"]["movies"].find({})]
+    else:
+        start_urls = ("https://www.imdb.com/title/tt20850406/?ref_=tt_sims_tt_t_1", "https://www.imdb.com/title/tt0241527/",
+                      "https://www.imdb.com/title/tt0266543/?ref_=hm_tpks_tt_i_12_pd_tp1_pbr_ic",
+                      "https://www.imdb.com/title/tt2788316/?ref_=hm_top_tt_i_5",
+                      "https://www.imdb.com/title/tt10919420/?ref_=hm_tpks_tt_i_15_pd_tp1_pbr_ic",
+                      "https://www.imdb.com/title/tt0382932/?ref_=hm_tpks_tt_i_18_pd_tp1_pbr_ic",
+                      "https://www.imdb.com/title/tt1632701/?ref_=hm_tpks_tt_i_26_pd_tp1_pbr_ic",
+                      "https://www.imdb.com/title/tt14230458/?ref_=hm_top_tt_i_2",
+                      "https://www.imdb.com/title/tt15239678/?ref_=hm_top_tt_i_3",
+                      "https://www.imdb.com/title/tt0068646/?ref_=nv_sr_srsg_3_tt_4_nm_4_q_godf",
+                      "https://www.imdb.com/title/tt0108052/?ref_=hm_tpks_tt_i_4_pd_tp1_pbr_ic")
+    
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
     }
@@ -35,7 +40,7 @@ class IMDBSpider(scrapy.Spider):
         collection = db["movies2"]
         # iterate movie sections
         data = {"_id": get_id(response.url)}
-        if collection.count_documents({'_id': data["_id"]}) == 0:
+        if update or collection.count_documents({'_id': data["_id"]}) == 0:
             data['title'] = response.css('.hero__primary-text::text').get().strip()
 
             genres = response.xpath(
@@ -47,7 +52,10 @@ class IMDBSpider(scrapy.Spider):
                 "//li[contains(.//span, 'Director')]/div/ul/li/a/text()").extract() or None
             if directors:
                 data['directors'] = list(set(directors))
-
+            data["type"] = response.xpath(
+                "//meta[contains(@property, 'og:type')]/@content").extract() or None
+            data["image"] = response.xpath(
+                "//meta[contains(@property, 'og:image')]/@content").extract() or None
             data["origin"] = response.xpath(
                 "//*[contains(.//*, 'Country of origin')]/div/ul/li/a/text()").extract() or None
             data["Runtime"] = response.xpath(
@@ -64,12 +72,18 @@ class IMDBSpider(scrapy.Spider):
                 "//*[contains(.//*, 'Stars')]/div/ul/li/a/text()").extract() or None
             if stars:
                 data['stars'] = list(set(stars))
-            collection.insert_one(data)
+            if update:
+                id = data["_id"]
+                del data["_id"]
+                collection.update_one({"_id":id},{"_set":data},{upsert:True})
+            else:
+                collection.insert_one(data)
             del data
-            for link in response.css(".ipc-poster-card__title"):
-                url = f"https://www.imdb.com{link.attrib['href']}"
-                id = get_id(url)
-                if collection.count_documents({'_id': id}) < 1:
-                    del id
-                    yield Request(url, callback=self.parse)
+            if not update:
+                for link in response.css(".ipc-poster-card__title"):
+                    url = f"https://www.imdb.com{link.attrib['href']}"
+                    id = get_id(url)
+                    if collection.count_documents({'_id': id}) < 1:
+                        del id
+                        yield Request(url, callback=self.parse)
         yield None
